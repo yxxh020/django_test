60bab775961afefd32daf6c5bee9a7ed43e040d1
yrhwang <yrhwang@softnet.co.kr>
2023-07-20 14:18:11 +0900

    Post 모델 생성 후 타음존 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model): #models모듈의 Model 클래스 사용
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True) #처음 작성 시간 자동저장
+    updated_at = models.DateTimeField(auto_now=True) # 수정시간 자동 저장
+#     author:
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
