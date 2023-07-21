02dc8de9d3b633e34774a580b15d735baa005bb4
yxxh020 <yiran023h@gmail.com>
2023-07-21 10:08:38 +0900

    models.py에 주석 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model): #models모듈의 Model 클래스 사용
@@ -7,7 +8,22 @@ class Post(models.Model): #models모듈의 Model 클래스 사용
 
     created_at = models.DateTimeField(auto_now_add=True) #처음 작성 시간 자동저장
     updated_at = models.DateTimeField(auto_now=True) # 수정시간 자동 저장
+
+    '''
+    DateTimeField 옵션
+    auto_now: 모델 객체가 저장될 때마다 현재 시간으로 업데이트
+    auto_now_add: 처음 생성될 때( 새로운 레코드가 DB에 추가될 때) 한번만 값을 할당
+    auto_created: 공식문서에도 설명 없음. 사용자가 create 시간 설정 가능
+    '''
 #     author:
 
-    def __str__(self):
-        return f'[{self.pk}] {self.title}'
+    def __str__(self): #제목에 object로 나와서 문자열로 변환 뒤 출력
+        return f'[{self.pk}] {self.title} ' f'{self.updated_at}'[:-7]
+
+    '''
+    자주 쓰는 내장 함수
+    __str__(): 해당 클래스로 만들어진 인스턴트(변수)를 문자열로 출력해주는 메소드
+    __repr__(): represent. 해당 객체를 문자열로 설명
+    __init__(): 객체를 만들 때 처음 실행되는 초기화 메소드. 생성자로 인식되어 객체가 생성되는 시점에 자동으로 호출 됨.
+    
+    '''
