09f062b0325eca7def37efae258c79328b0d65a7
yxxh020 <yiran023h@gmail.com>
2023-07-21 16:59:26 +0900

    CBV로 블로그 포스트 목록 페이지 생성

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    # path('', views.index),  # FBV(function based view)
+    path('', views.PostList.as_view()),  # CBV(class based view)
     path('<int:pk>/', views.single_post_page),
-    path('', views.index),  # function based view(FBV)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
-# Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')  # 모든 데이터를 다 가져오는 ORM 쿼리
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
+# FBV(function based view 방식)
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')  # 모든 데이터를 다 가져오는 ORM 쿼리
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,  # 딕셔너리
+#         }
+#     )
+#
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
@@ -24,3 +25,16 @@ def single_post_page(request, pk):
             'post': post,
         }
     )
+
+# CBV(class based view 방식)
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/post_list.html'
+    ordering = '-pk'
+
+    '''
+    ListView 디폴트 속성
+    1) context 변수: object_list 또는 모델명_list
+    2) template: 모델명_list.html
+        ex) post_list.html
+    '''
